27b9c2512f1b7c137bae4203a06f250e2219c5dc
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2023-02-16 22:11:25 +0100

    exercice

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// print numbers from n to 1 using recursion
+namespace Program;
+
+class FromNToOneTask{
+
+public static int FromNToOne(int num){
+
+if (num < 1){
+    return num;
+}
+
+Console.Write (num + " ");
+num--;
+return FromNToOne(num);
+}
+
+public static void Main (string[] args) {
+
+    FromNToOne(10);
+}
+
+
+
+}
